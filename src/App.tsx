4f81092860f7fcc84edd68e427d02ce4fb77498e
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2024-07-31 06:36:48 -0500

    Is it working?

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { RouterR } from "./components/RouterR";
 import { ShowQuote } from "./components/ShowQuote";
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
 
   return (
     <>
+      <RouterR/>
       <h1>I am the best fucking menu in the world</h1>
       <ShowQuote/>
     </>
